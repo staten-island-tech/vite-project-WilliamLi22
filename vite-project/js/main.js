fd3ad1413230254f35c2be7ed2cb65976f32bca5
WilliamLi22 <forvalious@gmail.com>
2022-12-08 10:16:40 -0500

    Started video 3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,20 @@
 import "../styles/style.css";
 import { menu } from "./menu";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 document.querySelector("#app").innerHTML = `<h1>Hello</h1> 
 <a href="https:vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
 
 console.log(menu);
+
+//create an array of object (menu)
+//create function to inject mennu item
+//iterate (foreach) over array and insert HTML
+//button event listen to filter
+
+//OOP Object oriented programming to encapsulate data/functions and create abstractions (when you take a bunch of code and create a one word function for it, makes code reusable)
+
+//
