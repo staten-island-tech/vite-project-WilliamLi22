3df83e62061d0a329dc6c3c263fc8971320056ab
WilliamLi22 <forvalious@gmail.com>
2022-12-16 10:15:15 -0500

    Start video 3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -5,9 +5,6 @@ import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 
-document.querySelector("#app").innerHTML = `<h1>Hello</h1> 
-<a href="https:vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
-
 console.log(menu);
 
 //create an array of object (menu)
