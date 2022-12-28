058cc76704e2cc109ab2818eb6db16a783d73e6b
WilliamLi22 <forvalious@gmail.com>
2022-12-27 19:10:50 -0500

    Fixed Main color not going all the way, added some buttons (Still need to add filtering)

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,4 +1,8 @@
 const DOMSelectors = {
+  vegetarian: document.getElementById("vegetarian"),
+  drink: document.getElementById("drink"),
+  popular: document.getElementById("popular"),
+  budget: document.getElementById("budget"),
   box: document.getElementById("box"),
 };
 
