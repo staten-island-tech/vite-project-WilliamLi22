8e05fcc5d2505fd045395ccd0252dac7d501fb7f
WilliamLi22 <forvalious@gmail.com>
2022-12-31 12:36:58 -0500

    Removed extra cards, not filtering

--- vite-project/js/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("sadfood")) {
-    document.body.classList.add("fastfood");
-    document.body.classList.remove("sadfood");
-  } else {
-    document.body.classList.add("sadfood");
-    document.body.classList.remove("fastfood");
-  }
-});
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,39 +1,49 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
-import "../js/index.js";
+/* import "../js/index.js"; */
 import { menu } from "./menu";
 
-console.log(menu);
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("sadfood")) {
+    document.body.classList.add("fastfood");
+    document.body.classList.remove("sadfood");
+  } else {
+    document.body.classList.add("sadfood");
+    document.body.classList.remove("fastfood");
+  }
+});
 
-function card() {
-  menu.forEach((food) => {
+function all() {
+  menu.forEach((menu) => {
     DOMSelectors.box.insertAdjacentHTML(
       "beforeend",
       `<div class="inner">
-          <h2>${food.name}</h2>
-          <img class="foodimg" src="${food.img}" alt="">
-          <h3>${food.price} Dollars</h3>
+          <h2>${menu.name}</h2>
+          <img class="foodimg" src="${menu.img}" alt="">
+          <h3>${menu.price} Dollars</h3>
           </div>`
     );
   });
 }
 
+all();
+
 function filterPopular() {
   menu
-    .filter((food) => food.popular == "yes")
-    .forEach((food) => {
+    .filter((menu) => menu.popular == "yes")
+    .forEach((menu) => {
       DOMSelectors.box.insertAdjacentHTML(
         "beforeend",
         `<div class="inner">
-          <h2>${food.name}</h2>
-          <img class="foodimg" src="${food.img}" alt="">
-          <h3>${food.price} Dollars</h3>
+          <h2>${menu.name}</h2>
+          <img class="foodimg" src="${menu.img}" alt="">
+          <h3>${menu.price} Dollars</h3>
           </div>`
       );
     });
 }
 
-function filterVegetarian(e) {
+function filterVegetarian() {
   e.filter((menu) => menu.vegetarian == "yes");
   e.forEach((menu) => {
     DOMSelectors.box.insertAdjacentHTML(
@@ -47,7 +57,7 @@ function filterVegetarian(e) {
   });
 }
 
-function filterBudget(e) {
+function filterBudget() {
   e.filter((menu) => menu.budget == "yes");
   e.forEach((menu) => {
     DOMSelectors.box.insertAdjacentHTML(
@@ -61,7 +71,7 @@ function filterBudget(e) {
   });
 }
 
-function filterDrinks(e) {
+function filterDrinks() {
   e.filter((menu) => menu.drink == "yes");
   e.forEach((menu) => {
     DOMSelectors.box.insertAdjacentHTML(
@@ -75,11 +85,34 @@ function filterDrinks(e) {
   });
 }
 
-card(menu);
-filterPopular(menu);
-filterVegetarian(menu);
-filterBudget(menu);
-filterDrinks(menu);
+function deleteAll() {
+  DOMSelectors.menu.innerHTML = "";
+}
+
+document.getElementById("all").addEventListener("click", function () {
+  clear();
+  all();
+});
+
+document.getElementById("vegetarian").addEventListener("click", function () {
+  clear();
+  filterVegetarian();
+});
+
+document.getElementById("budget").addEventListener("click", function () {
+  clear();
+  filterBudget();
+});
+
+document.getElementById("popular").addEventListener("click", function () {
+  clear();
+  filterPopular();
+});
+
+document.getElementById("drinks").addEventListener("click", function () {
+  clear();
+  filterDrinks();
+});
 
 //create an array of object (menu)
 //create function to inject mennu item
